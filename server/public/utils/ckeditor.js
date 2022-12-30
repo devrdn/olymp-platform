77fa3c62a014cca41c8e201be614671b21818418
/rdndev <rdninc@mail.ru>
2022-12-30 23:23:40 +0200

    feat: add table in ckeditor

--- server/public/utils/ckeditor.js
+++ server/public/utils/ckeditor.js
@@ -1,5 +1,16 @@
 const defaultConfig = {
-  toolbar: ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote'],
+  toolbar: [
+    'insertTable',
+    'heading',
+    '|',
+    'bold',
+    'italic',
+    'link',
+    'bulletedList',
+    'numberedList',
+    'blockQuote',
+    'imageUpload',
+  ],
   heading: {
     options: [
       { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
@@ -7,6 +18,18 @@ const defaultConfig = {
       { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
       { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
       { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+    ],
+  },
+  table: {
+    contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
+  },
+  image: {
+    toolbar: [
+      'imageStyle:block',
+      'imageStyle:side',
+      '|',
+      'toggleImageCaption',
+      'imageTextAlternative',
     ],
   },
 };
