a6088c1130a7a9bf5d85d0b453bb7fba082b7cea
/rdndev <rdninc@mail.ru>
2023-02-13 00:48:18 +0200

    refactor: merge controllers
    Merge LoginController and RegisterController in SecurityController

--- server/src/Controller/Security/AuthController.php
+++ server/src/Controller/Security/AuthController.php
@@ -1,24 +1,33 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Security;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
-use DateTimeImmutable;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-/**
- * Controller responsible for user registration
- */
-class RegistrationController extends AbstractController
+class AuthController extends AbstractController
 {
-    // todo: add email verify and user ip 
+    #[Route('/login', name: 'app_login', methods: ["POST", "GET"])]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    { 
+        // get login error
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // get last email
+        $lastEmail = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_email' => $lastEmail,
+            'auth_error' => $error,
+        ]);
+    }
 
     #[Route('/register', name: 'app_register', methods: ["POST", "GET"])]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserRepository $userRepository): Response
@@ -47,8 +56,16 @@ class RegistrationController extends AbstractController
         }
 
         // render registration form
-        return $this->render('registration/register.html.twig', [
+        return $this->render('security/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ["GET"])]
+    public function logout(): Response
+    {
+        // controller can't be empty, so ...
+        // return new response!
+        return new Response("You are cool, bro!");
     }
 }
