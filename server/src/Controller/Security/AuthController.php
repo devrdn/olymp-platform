f65e41d7ded17b7e78e14e080d45ec6d9ec69623
/rdndev <rdninc@mail.ru>
2023-04-21 16:43:58 +0300

    refactor: separate auth and register controllers

--- server/src/Controller/Security/AuthController.php
+++ server/src/Controller/Security/AuthController.php
@@ -2,13 +2,9 @@
 
 namespace App\Controller\Security;
 
-use App\Entity\User;
-use App\Form\RegistrationFormType;
-use App\Repository\UserRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -26,38 +22,6 @@ class AuthController extends AbstractController
         return $this->render('security/login.html.twig', [
             'last_email' => $lastEmail,
             'auth_error' => $error,
-        ]);
-    }
-
-    #[Route('/register', name: 'app_register', methods: ["POST", "GET"])]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserRepository $userRepository): Response
-    {
-        $user = new User();
-
-        // create form and handle user request
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-
-            // set created at date
-            $user->setCreatedAt(new \DateTimeImmutable());
-
-            $userRepository->save($user, true);
-            // do anything else you need here, like send an email
-            return $this->redirectToRoute('app_task_list');
-        }
-
-        // render registration form
-        return $this->render('security/register.html.twig', [
-            'registrationForm' => $form->createView(),
         ]);
     }
 
---
+++ server/src/Controller/Security/RegistrationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller\Security;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register', methods: ["POST", "GET"])]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserRepository $userRepository): Response
+    {
+        $user = new User();
+
+        // create form and handle user request
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            // set created at date
+            $user->setCreatedAt(new \DateTimeImmutable());
+
+            $userRepository->save($user, true);
+            // do anything else you need here, like send an email
+            return $this->redirectToRoute('app_task_list');
+        }
+
+        // render registration form
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+}
