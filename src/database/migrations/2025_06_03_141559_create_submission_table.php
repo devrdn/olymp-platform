0ff94443ae9ff275f3b4f96d92e038b134c34720
Nichita Nartea <rdninc@mail.ru>
2025-06-03 17:25:42 +0300

    feat: create migration for submission table with user_id foreign key and path field

---
+++ src/database/migrations/2025_06_03_141559_create_submission_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('submission', function (Blueprint $table) {
+            $table->id();
+            $table->foregin('user_id')
+                ->constrained('tasks')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->text('path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('submission');
+    }
+};
